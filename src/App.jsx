e29c57b05de503f0d537ca94ff172b3abfd990ba
syeda <syedahadiqa2@gmail.com>
2024-07-31 12:57:11 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+
+
+import { useState, useEffect } from 'react';
+import Header from "./components/header.jsx";
+import Form from "./components/form.jsx";
+import TodoList from "./components/Todolist.jsx";
+import './App.css';
+
+const App = () => {
+  const initialState = JSON.parse(localStorage.getItem('todos')) || [];
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <div className='container'>
+      <div className='app-wrapper'>
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+        <div>
+          <TodoList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from"react";
+
+const Header = () => {
+return (
+    <div className="Header">
+        <h1>ToDos List</h1>
+    </div>
+)
+}
+
+export default Header; 
